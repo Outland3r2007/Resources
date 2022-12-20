8f18e4b6beca575db88072932a22d9092f6e01c7
Balogh Gabriel Claudiu(Outland3r2007) <baloggabriel76@outlook.com>
2022-12-20 07:44:26 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// Dom7
+var $ = Dom7;
+
+// Theme
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+    theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+// Init App
+var app = new Framework7({
+    id: 'io.framework7.testapp',
+    root: '#app',
+    theme: theme,
+    data: function() {
+        return {
+            user: {
+                firstName: 'John',
+                lastName: 'Doe',
+            },
+        };
+    },
+    methods: {
+        helloWorld: function() {
+            app.dialog.alert('Hello World!');
+        },
+    },
+    routes: routes,
+    popup: {
+        closeOnEscape: true,
+    },
+    sheet: {
+        closeOnEscape: true,
+    },
+    popover: {
+        closeOnEscape: true,
+    },
+    actions: {
+        closeOnEscape: true,
+    },
+    vi: {
+        placementId: 'pltd4o7ibb9rc653x14',
+    },
+});
